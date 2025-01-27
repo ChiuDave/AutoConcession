17dc33ea8bbd5ac568b4fb1608e4c70d39e14b76
ChiuDave <e2243115@cmaisonneuve.qc.ca>
2025-01-27 10:54:08 -0500

    added navbar

---
+++ car_ai/src/composants/Navbar.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+
+const Navbard = () => {
+
+  return (
+    <div style={{ display: "flex", height: "100vh" }}>
+      <h1>Navbard</h1>
+    </div>
+  );
+};
+
+export default Navbard;
