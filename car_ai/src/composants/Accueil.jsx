5c70190976a97a8398742228f36e74e161ad71e7
ChiuDave <e2243115@cmaisonneuve.qc.ca>
2025-01-27 16:55:58 -0500

    Added text

--- car_ai/src/composants/Accueil.jsx
+++ car_ai/src/composants/Accueil.jsx
@@ -9,6 +9,10 @@ const Accueil = () => {
        {/* Main content */}
        <div style={{ marginTop: "60px", padding: "20px" }}>
         <h1>Accueil</h1>
+        <h1>Un nouveau marché révolutionnaire</h1>
+        <div style={{}}> 
+          <h3>Un nouveau moyen pour effectuer vos achat en ligne. Notre petit robot vous aidera à choisir la voiture de vos rêves selon vos préférence</h3>
+        </div>
       </div>
     </div>
   );
