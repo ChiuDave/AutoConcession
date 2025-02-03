babad937b7ec824971dd056f18c68e079f9be6df
dvp536 <missdegat@hotmail.com>
2025-02-03 10:15:15 -0500

    Fix route and add chatbot component

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Accueil from './composants/Accueil/Accueil';
 import Dashboard from './composants/DashBoard/Dashboard';
 import Details from './composants/Details/Details';
+import ChatBot from "./composants/Chat/ChatBot";
 
 function App() {
 
   return (
     <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Accueil />} />
-                <Route path="/dashboard" element={<Dashboard />}/>
-                <Route path="/details/:id" element={<Details />}/>
-            </Routes>
-        </BrowserRouter>
+        <Routes>
+            <Route path="/VoitureAI/" element={<Accueil />} />
+            <Route path="/VoitureAI/dashboard" element={<Dashboard />}/>
+            <Route path="/VoitureAI/details/:id" element={<Details />}/>
+            <Route path="/VoitureAI/chatbot" element={<ChatBot/>}/>
+        </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/composants/Navbar/Navbar.jsx
+++ src/composants/Navbar/Navbar.jsx
@@ -10,10 +10,10 @@ const Navbar = () => {
 
         {/* Navigation Links - Now Horizontal */}
         <ul className="flex space-x-6">
-          <li><Link to="/" className="text-gray-700 hover:text-blue-600">Accueil</Link></li>
-          <li><Link to="/dashboard" className="text-gray-700 hover:text-blue-600">Voitures</Link></li>
-          <li><Link to="/about" className="text-gray-700 hover:text-blue-600">À propos</Link></li>
-          <li><Link to="/contact" className="text-gray-700 hover:text-blue-600">Contact</Link></li>
+          <li><Link to="/VoitureAI/" className="text-gray-700 hover:text-blue-600">Accueil</Link></li>
+          <li><Link to="/VoitureAI/dashboard" className="text-gray-700 hover:text-blue-600">Voitures</Link></li>
+          <li><Link to="/VoitureAI/about" className="text-gray-700 hover:text-blue-600">À propos</Link></li>
+          <li><Link to="/VoitureAI/contact" className="text-gray-700 hover:text-blue-600">Contact</Link></li>
         </ul>
       </div>
     </nav>
