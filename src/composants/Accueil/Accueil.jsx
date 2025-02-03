52df67a720050a238e79557a4813824d3978742f
dvp536 <missdegat@hotmail.com>
2025-02-03 10:04:49 -0500

    Fix import in files

--- src/composants/Accueil/Accueil.jsx
+++ src/composants/Accueil/Accueil.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Navbar from "./Navbar";
+import Navbar from "../Navbar/Navbar";
 
 const Accueil = () => {
   const [showNavbar, setShowNavbar] = useState(true);
