c2a78288c10831175e9ee2d93ff0c48fea202492
dvp536 <missdegat@hotmail.com>
2025-02-16 22:49:14 -0500

    Revert the filter changes

--- src/composants/DashBoard/Filters/Filters.jsx
+++ src/composants/DashBoard/Filters/Filters.jsx
@@ -5,16 +5,11 @@ const Filters = ({ data, filters, setFilters }) => {
     localStorage.setItem(e.target.name, e.target.value);
   };
 
-  // Filter models based on the selected make
-  const filteredModels = filters.make
-    ? [...new Set(data.filter(car => car.Make === filters.make).map(car => car.Model))].sort()
-    : [...new Set(data.map(car => car.Model))].sort();
-
   return (
     <div className="w-full max-w-6xl mt-4 grid grid-cols-5 gap-4 bg-white p-4 rounded-lg shadow-md">
       {[
         { label: "Marque", name: "make", options: [...new Set(data.map((car) => car.Make))].sort(), default: "Toutes les marques" },
-        { label: "Model", name: "name", options: filteredModels, default: "Toutes les models" },
+        { label: "Model", name: "name", options: [...new Set(data.map(car => car.Model))].sort(), default: "Toutes les models" },
         { label: "Année", name: "year", options: [...new Set(data.map((car) => car.Year))].sort((a, b) => a - b), default: "Toutes les années" },
         { label: "Couleur Extérieure", name: "exteriorColor", options: [...new Set(data.map((car) => car.Ext_Color_Generic))].filter(Boolean).sort(), default: "Toutes les couleurs" },
         { label: "Couleur Intérieure", name: "interiorColor", options: [...new Set(data.map((car) => car.Int_Color_Generic))].filter(Boolean).sort(), default: "Toutes les couleurs" },
